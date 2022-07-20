1cb6fe7f87bbe07467ee8c4cc864323f1e03c8ac
René Gutiérrez <t1013600221@unitru.edu.pe>
2022-07-19 23:33:13 -0500

    listo

---
+++ PREGUNTA2.py
@@ -0,0 +1,16 @@
+numero_de_listas = int(input("Cuantas listas quiere escribir?"))
+for j in range(numero_de_listas):
+    print("Dígame cuántas palabras tiene la lista", j+1)
+    numero = int(input())
+    if numero<1:
+        print("error")
+    else:
+       lista = []
+       for i in range(numero):
+        print("Dígame la palabra", str(i+1)+":", end="")
+        palabra = input()
+        lista += [palabra]
+    print("La lista", j+1 ,"creada es:", lista)
+     
+
+  
